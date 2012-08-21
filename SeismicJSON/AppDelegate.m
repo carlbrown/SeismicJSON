8f55b1ae76492244847588cef8287a96328602c0
Carl Brown <carlb@PDAgent.com>
2012-08-21 15:10:08 -0500

    Switch to in-memory store type so we can see freshly loaded data each time

--- SeismicJSON/AppDelegate.m
+++ SeismicJSON/AppDelegate.m
@@ -131,7 +131,7 @@
     
     NSError *error = nil;
     _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSInMemoryStoreType configuration:nil URL:storeURL options:nil error:&error]) {
         /*
          Replace this implementation with code to handle the error appropriately.
          
