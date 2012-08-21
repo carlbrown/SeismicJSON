a5f8f6a6aca38f34c65e6b056a318ad3b908e649
Carl Brown <carlb@PDAgent.com>
2012-08-21 14:54:15 -0500

    MIT License

--- SeismicJSONTests/SeismicJSONTests.h
+++ SeismicJSONTests/SeismicJSONTests.h
@@ -3,7 +3,7 @@
 //  SeismicJSONTests
 //
 //  Created by Carl Brown on 8/21/12.
-//  Copyright (c) 2012 PDAgent. All rights reserved.
+//  Copyright (c) 2012 PDAgent. Released under MIT license ( http://opensource.org/licenses/MIT ).
 //
 
 #import <SenTestingKit/SenTestingKit.h>
