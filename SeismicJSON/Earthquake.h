949b9c68b024edb6f44bd5575ae60ae48af2a11b
Carl Brown <carlb@PDAgent.com>
2012-08-21 15:04:25 -0500

    NSManagedObject Subclass

---
+++ SeismicJSON/Earthquake.h
@@ -0,0 +1,22 @@
+//
+//  Earthquake.h
+//  SeismicJSON
+//
+//  Created by Carl Brown on 8/21/12.
+//  Copyright (c) 2012 PDAgent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Earthquake : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longitude;
+@property (nonatomic, retain) NSString * webLinkToUSGS;
+@property (nonatomic, retain) NSDate * date;
+@property (nonatomic, retain) NSString * location;
+@property (nonatomic, retain) NSNumber * magnitude;
+
+@end
---
+++ SeismicJSON/Earthquake.m
@@ -0,0 +1,21 @@
+//
+//  Earthquake.m
+//  SeismicJSON
+//
+//  Created by Carl Brown on 8/21/12.
+//  Copyright (c) 2012 PDAgent. All rights reserved.
+//
+
+#import "Earthquake.h"
+
+
+@implementation Earthquake
+
+@dynamic latitude;
+@dynamic longitude;
+@dynamic webLinkToUSGS;
+@dynamic date;
+@dynamic location;
+@dynamic magnitude;
+
+@end
