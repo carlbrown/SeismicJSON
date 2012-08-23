296efa1e6620154a314930fba89ff8acde385d5e
Carl Brown <carlb@PDAgent.com>
2012-08-23 08:19:28 -0500

    Use early returns to simplify depth of the conditional trees

--- SeismicJSON/EarthquakeFetchOperation.m
+++ SeismicJSON/EarthquakeFetchOperation.m
@@ -18,7 +18,7 @@
         NSLog(@"Cannot start without a Primary Managed Object Context");
         return;
     }
-
+    
     [super main];
 }
 
@@ -28,100 +28,117 @@
         return;
     }
     
-    if (self.response.statusCode==200) {
-        
-        NSError *error=nil;
-        
-        
-        id objectFromJSON = [NSJSONSerialization JSONObjectWithData:self.fetchedData options:0 error:&error];
-        if (objectFromJSON) {
-#if kUSE_NSNOTIFICATIONS_FOR_CONTEXT_MERGE
-            NSManagedObjectContext *context = [[NSManagedObjectContext alloc] init];
-            [context setPersistentStoreCoordinator:self.mainContext.persistentStoreCoordinator];
-#else
-            NSManagedObjectContext *context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
-            [context setParentContext:[self mainContext]];
-#endif
-            
-            
-            if ([objectFromJSON isKindOfClass:[NSDictionary class]]) {
-                
-                NSDictionary *jsonDict = (NSDictionary *) objectFromJSON;
-                
-                
-                if ([[jsonDict objectForKey:@"features"] isKindOfClass:[NSArray class]]) {
-                    
-                    NSArray *events = [jsonDict objectForKey:@"features"];
-                    
-                    //Keep track of if we need to save or not
-                    BOOL foundNewObject=NO;
-                    
-                    for (NSObject *arrayElement in events) {
-                        
-                        if ([arrayElement isKindOfClass:[NSDictionary class]]) {
-                            NSDictionary *eventDict = (NSDictionary *) arrayElement;
-                            
-                            
-                            NSString *eventLocation = [eventDict valueForKeyPath:@"properties.place"];
-                            NSDate *eventDate = [NSDate dateWithTimeIntervalSince1970:[[eventDict valueForKeyPath:@"properties.time"] doubleValue]];
-                            NSNumber *eventLong = [NSNumber numberWithDouble:[[[eventDict valueForKeyPath:@"geometry.coordinates"] objectAtIndex:0] doubleValue]];
-                            NSNumber *eventLat =[NSNumber numberWithDouble:[[[eventDict valueForKeyPath:@"geometry.coordinates"] objectAtIndex:1] doubleValue]];
-                            NSNumber *eventMagnitude = [NSNumber numberWithFloat:[[eventDict valueForKeyPath:@"properties.mag"] floatValue]];
-                            NSString *eventWebPath = [@"http://earthquake.usgs.gov" stringByAppendingPathComponent:[eventDict valueForKeyPath:@"properties.url"]];
-                            
-                            NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([Earthquake class])];
-                            [fetchRequest setFetchLimit:1];
-                            NSPredicate *eventInfo = [NSPredicate predicateWithFormat:@"location = %@ AND date = %@",
-                                                      eventLocation,
-                                                      eventDate];
-                            [fetchRequest setPredicate:eventInfo];
-                            NSError *fetchError=nil;
-                            NSArray *existingEventsMatchingThisOne = [context executeFetchRequest:fetchRequest error:&fetchError];
-                            if (existingEventsMatchingThisOne==nil) {
-                                NSLog(@"Error checking for existing record: %@",[fetchError localizedDescription]);
-                            } else if ([existingEventsMatchingThisOne count]==0) {
-                                
-                                //Didn't find one already, make a new one
-                                NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([Earthquake class]) inManagedObjectContext:context];
-                                
-                                [newManagedObject setValue:eventLocation forKey:@"location"];
-                                [newManagedObject setValue:eventDate forKey:@"date"]; 
-                                [newManagedObject setValue:eventLat forKey:@"latitude"];
-                                [newManagedObject setValue:eventLong forKey:@"longitude"];
-                                [newManagedObject setValue:eventMagnitude forKey:@"magnitude"];
-                                [newManagedObject setValue:eventWebPath forKey:@"webLinkToUSGS"];
-                                
-                                foundNewObject=YES; //remember we need to save now
-                            }
-                            
-                        }
-                    }
-                    
-                    if (foundNewObject) {
-                        
-                        // Save the context.
-                        error = nil;
-                        if (![context save:&error]) {
-                            // Replace this implementation with code to handle the error appropriately.
-                            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-                            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-                            abort();
-                        }
-#if kUSE_PARENT_CONTEXTS_FOR_CONTEXT_MERGE
-                        dispatch_sync(dispatch_get_main_queue(), ^{
-                            NSError *error = nil;
-                            if (![self.mainContext save:&error]) {
-                                // Replace this implementation with code to handle the error appropriately.
-                                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-                                NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-                                abort();
-                            }
-                        });
-#endif
-                    }
-                }
-            }
+    if (self.response.statusCode!=200) {
+        NSLog(@"Bad HTTP response code: %d[%@]",self.response.statusCode,[NSHTTPURLResponse localizedStringForStatusCode:self.response.statusCode]);
+        [self finish];
+        return;
+    }
+    
+    
+    NSError *error=nil;
+    
+    
+    id objectFromJSON = [NSJSONSerialization JSONObjectWithData:self.fetchedData options:0 error:&error];
+    if (!objectFromJSON) {
+        NSLog(@"JSON parsing error:%@/%@",[error localizedDescription], [error userInfo]);
+        if (self.delegate) {
+            [self.delegate fetchDidFailWithError:error];
         }
+        [self finish];
+        return;
+    }
+#if kUSE_NSNOTIFICATIONS_FOR_CONTEXT_MERGE
+    NSManagedObjectContext *context = [[NSManagedObjectContext alloc] init];
+    [context setPersistentStoreCoordinator:self.mainContext.persistentStoreCoordinator];
+#else
+    NSManagedObjectContext *context = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    [context setParentContext:[self mainContext]];
+#endif
+    
+    
+    if (![objectFromJSON isKindOfClass:[NSDictionary class]]) {
+        NSLog(@"Unexpected JSON element. Expected NSDictionary, got %@",[objectFromJSON class]);
+        [self finish];
+        return;
+    }
+    
+    NSDictionary *jsonDict = (NSDictionary *) objectFromJSON;
+    
+    
+    if (![[jsonDict objectForKey:@"features"] isKindOfClass:[NSArray class]]) {
+        NSLog(@"Unexpected JSON element. Expected NSArray, got %@",[[jsonDict objectForKey:@"features"] class]);
+        [self finish];
+        return;
+    }
+    
+    
+    NSArray *events = [jsonDict objectForKey:@"features"];
+    
+    //Keep track of if we need to save or not
+    BOOL foundNewObject=NO;
+    
+    for (NSObject *arrayElement in events) {
+        
+        if ([arrayElement isKindOfClass:[NSDictionary class]]) {
+            NSDictionary *eventDict = (NSDictionary *) arrayElement;
+            
+            
+            NSString *eventLocation = [eventDict valueForKeyPath:@"properties.place"];
+            NSDate *eventDate = [NSDate dateWithTimeIntervalSince1970:[[eventDict valueForKeyPath:@"properties.time"] doubleValue]];
+            NSNumber *eventLong = [NSNumber numberWithDouble:[[[eventDict valueForKeyPath:@"geometry.coordinates"] objectAtIndex:0] doubleValue]];
+            NSNumber *eventLat =[NSNumber numberWithDouble:[[[eventDict valueForKeyPath:@"geometry.coordinates"] objectAtIndex:1] doubleValue]];
+            NSNumber *eventMagnitude = [NSNumber numberWithFloat:[[eventDict valueForKeyPath:@"properties.mag"] floatValue]];
+            NSString *eventWebPath = [@"http://earthquake.usgs.gov" stringByAppendingPathComponent:[eventDict valueForKeyPath:@"properties.url"]];
+            
+            NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([Earthquake class])];
+            [fetchRequest setFetchLimit:1];
+            NSPredicate *eventInfo = [NSPredicate predicateWithFormat:@"location = %@ AND date = %@",
+                                      eventLocation,
+                                      eventDate];
+            [fetchRequest setPredicate:eventInfo];
+            NSError *fetchError=nil;
+            NSArray *existingEventsMatchingThisOne = [context executeFetchRequest:fetchRequest error:&fetchError];
+            if (existingEventsMatchingThisOne==nil) {
+                NSLog(@"Error checking for existing record: %@",[fetchError localizedDescription]);
+            } else if ([existingEventsMatchingThisOne count]==0) {
+                
+                //Didn't find one already, make a new one
+                NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([Earthquake class]) inManagedObjectContext:context];
+                
+                [newManagedObject setValue:eventLocation forKey:@"location"];
+                [newManagedObject setValue:eventDate forKey:@"date"]; 
+                [newManagedObject setValue:eventLat forKey:@"latitude"];
+                [newManagedObject setValue:eventLong forKey:@"longitude"];
+                [newManagedObject setValue:eventMagnitude forKey:@"magnitude"];
+                [newManagedObject setValue:eventWebPath forKey:@"webLinkToUSGS"];
+                
+                foundNewObject=YES; //remember we need to save now
+            }
+            
+        }
+    }
+    
+    if (foundNewObject) {
+        
+        // Save the context.
+        error = nil;
+        if (![context save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+#if kUSE_PARENT_CONTEXTS_FOR_CONTEXT_MERGE
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            NSError *error = nil;
+            if (![self.mainContext save:&error]) {
+                // Replace this implementation with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+                NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+                abort();
+            }
+        });
+#endif
     }
     [self finish];
 }
