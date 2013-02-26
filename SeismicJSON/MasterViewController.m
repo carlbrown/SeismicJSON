f0b18a22cfa3d5af473fff1b1c62bb8f6bee7956
Carl Brown <carlb@PDAgent.com>
2013-02-26 00:12:57 -0600

    remove unused edit button

--- SeismicJSON/MasterViewController.m
+++ SeismicJSON/MasterViewController.m
@@ -45,7 +45,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(showSelectorActionSheet:)];
     self.navigationItem.rightBarButtonItem = addButton;
