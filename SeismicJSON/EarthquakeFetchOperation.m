6ff2fb8cbeb90b56a0368bd7baa6e87b9a2736fd
Carl Brown <carlb@PDAgent.com>
2013-02-25 23:33:42 -0600

    Autorelease pool for performance

--- SeismicJSON/EarthquakeFetchOperation.m
+++ SeismicJSON/EarthquakeFetchOperation.m
@@ -76,67 +76,70 @@
     
     for (NSObject *arrayElement in events) {
         
-        if ([arrayElement isKindOfClass:[NSDictionary class]]) {
-            NSDictionary *eventDict = (NSDictionary *) arrayElement;
+        @autoreleasepool {
             
-            NSNumber *eventLong;
-            NSNumber *eventLat;
-            NSArray *coordArray = [eventDict valueForKeyPath:@"geometry.coordinates"];
-            if ([coordArray count]>=2) {
-                eventLong = [NSNumber numberWithDouble:[[coordArray objectAtIndex:0] doubleValue]];
-                eventLat =[NSNumber numberWithDouble:[[coordArray objectAtIndex:1] doubleValue]];
-            }
-
-            NSString *eventLocation = [eventDict valueForKeyPath:@"properties.place"];
-            NSDate *eventDate = [NSDate dateWithTimeIntervalSince1970:[[eventDict valueForKeyPath:@"properties.time"] doubleValue]];
-            NSNumber *eventMagnitude = [NSNumber numberWithFloat:[[eventDict valueForKeyPath:@"properties.mag"] floatValue]];
-            NSString *eventWebPath = [@"http://earthquake.usgs.gov" stringByAppendingPathComponent:[eventDict valueForKeyPath:@"properties.url"]];
-            
-            //Now, check in Core Data to see if we already have recorded this event
-            NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([Earthquake class])];
-            [fetchRequest setFetchLimit:1];
-            NSPredicate *eventInfo = [NSPredicate predicateWithFormat:@"location = %@ AND date = %@",
-                                      eventLocation,
-                                      eventDate];
-            [fetchRequest setPredicate:eventInfo];
-            NSError *fetchError=nil;
-            NSArray *existingEventsMatchingThisOne = [context executeFetchRequest:fetchRequest error:&fetchError];
-            if (existingEventsMatchingThisOne==nil) {
-                NSLog(@"Error checking for existing record: %@",[fetchError localizedDescription]);
-            } else if ([existingEventsMatchingThisOne count]>0) {
-                NSLog(@"Already had a copy of Event at %@. Not saving duplicate.",eventLocation);
-            } else {
+            if ([arrayElement isKindOfClass:[NSDictionary class]]) {
+                NSDictionary *eventDict = (NSDictionary *) arrayElement;
                 
-                //Didn't find one already, make a new one
-                NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([Earthquake class]) inManagedObjectContext:context];
-                
-                [newManagedObject setValue:eventLocation forKey:@"location"];
-                [newManagedObject setValue:eventDate forKey:@"date"]; 
-                [newManagedObject setValue:eventLat forKey:@"latitude"];
-                [newManagedObject setValue:eventLong forKey:@"longitude"];
-                [newManagedObject setValue:eventMagnitude forKey:@"magnitude"];
-                [newManagedObject setValue:eventWebPath forKey:@"webLinkToUSGS"];
-                
-                // Save the context.
-                error = nil;
-                if (![context save:&error]) {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-                    abort();
+                NSNumber *eventLong;
+                NSNumber *eventLat;
+                NSArray *coordArray = [eventDict valueForKeyPath:@"geometry.coordinates"];
+                if ([coordArray count]>=2) {
+                    eventLong = [NSNumber numberWithDouble:[[coordArray objectAtIndex:0] doubleValue]];
+                    eventLat =[NSNumber numberWithDouble:[[coordArray objectAtIndex:1] doubleValue]];
                 }
-#if kUSE_PARENT_CONTEXTS_FOR_CONTEXT_MERGE
-                dispatch_sync(dispatch_get_main_queue(), ^{
-                    NSError *error = nil;
-                    if (![self.mainContext save:&error]) {
+                
+                NSString *eventLocation = [eventDict valueForKeyPath:@"properties.place"];
+                NSDate *eventDate = [NSDate dateWithTimeIntervalSince1970:[[eventDict valueForKeyPath:@"properties.time"] doubleValue]];
+                NSNumber *eventMagnitude = [NSNumber numberWithFloat:[[eventDict valueForKeyPath:@"properties.mag"] floatValue]];
+                NSString *eventWebPath = [@"http://earthquake.usgs.gov" stringByAppendingPathComponent:[eventDict valueForKeyPath:@"properties.url"]];
+                
+                //Now, check in Core Data to see if we already have recorded this event
+                NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:NSStringFromClass([Earthquake class])];
+                [fetchRequest setFetchLimit:1];
+                NSPredicate *eventInfo = [NSPredicate predicateWithFormat:@"location = %@ AND date = %@",
+                                          eventLocation,
+                                          eventDate];
+                [fetchRequest setPredicate:eventInfo];
+                NSError *fetchError=nil;
+                NSArray *existingEventsMatchingThisOne = [context executeFetchRequest:fetchRequest error:&fetchError];
+                if (existingEventsMatchingThisOne==nil) {
+                    NSLog(@"Error checking for existing record: %@",[fetchError localizedDescription]);
+                } else if ([existingEventsMatchingThisOne count]>0) {
+                    NSLog(@"Already had a copy of Event at %@. Not saving duplicate.",eventLocation);
+                } else {
+                    
+                    //Didn't find one already, make a new one
+                    NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:NSStringFromClass([Earthquake class]) inManagedObjectContext:context];
+                    
+                    [newManagedObject setValue:eventLocation forKey:@"location"];
+                    [newManagedObject setValue:eventDate forKey:@"date"];
+                    [newManagedObject setValue:eventLat forKey:@"latitude"];
+                    [newManagedObject setValue:eventLong forKey:@"longitude"];
+                    [newManagedObject setValue:eventMagnitude forKey:@"magnitude"];
+                    [newManagedObject setValue:eventWebPath forKey:@"webLinkToUSGS"];
+                    
+                    // Save the context.
+                    error = nil;
+                    if (![context save:&error]) {
                         // Replace this implementation with code to handle the error appropriately.
                         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
                         abort();
                     }
-                });
+#if kUSE_PARENT_CONTEXTS_FOR_CONTEXT_MERGE
+                    dispatch_sync(dispatch_get_main_queue(), ^{
+                        NSError *error = nil;
+                        if (![self.mainContext save:&error]) {
+                            // Replace this implementation with code to handle the error appropriately.
+                            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+                            abort();
+                        }
+                    });
 #endif
-
+                    
+                }
             }
         }
     }
